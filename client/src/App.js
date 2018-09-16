01656d7e765b9c89cccb00adde6b0b1534df85db
Anastat <anastat@metropolia.fi>
2018-09-16 20:32:36 +0300

    changes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -26,8 +26,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Welcome to FlatSwap</h1>
         </header>
         <p className="App-intro">
           {this.state.response}
