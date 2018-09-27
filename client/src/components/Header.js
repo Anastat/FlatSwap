016f025cf30fd63b73e78d25619c3758ed252867
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2018-09-27 16:15:21 +0300

    Added HashRouter, NavLinks (no styling yet)
    not sure if this will be used with login and signup though

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import '../styles/Header.css';
 import logo from '../images/flatSwap_logo.png'
+import {
+	  Route,
+	  NavLink,
+	  HashRouter
+	} from "react-router-dom";
+import Host from "./Host";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const Header = () => {
     return (
+      <HashRouter>
       <div className="App-Header">
       <img className="logo" src={logo} alt="logo"/>
       <ul>
-        <li>Become a host</li>
-        <li>Login</li>
-        <li>Sign up</li>
+        <li><NavLink to="/hosting">Become a host</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/signup">Sign up</NavLink></li>
       </ul>
+      <div className="content">
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/hosting" component={Host}/>
     </div>
+    </div>
+    </HashRouter>
     )
 }
 
